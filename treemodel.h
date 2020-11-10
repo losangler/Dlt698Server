5522e367c2cf2e6bf6a3361cfbb298c2f9828bb1
jiemurphy <1925847831@qq.com>
2020-11-10 20:12:32 +0800

    设计了界面的主体框架，创建了输入、选择、列表等自定义组件

---
+++ treemodel.h
@@ -0,0 +1,29 @@
+#ifndef TREEMODEL_H
+#define TREEMODEL_H
+
+#include <QAbstractItemModel>
+
+class TreeModel : public QAbstractItemModel
+{
+    Q_OBJECT
+
+public:
+    explicit TreeModel(QObject *parent = 0);
+
+    // Header:
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    // Basic functionality:
+    QModelIndex index(int row, int column,
+                      const QModelIndex &parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex &index) const override;
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+private:
+};
+
+#endif // TREEMODEL_H
